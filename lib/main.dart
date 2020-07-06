c9a913c5d73a3122134d0e9d5535d5d4cf715d54
yuanxulong <1379035808@qq.com>
2020-07-06 23:51:50 +0800

    不知道咋整的热更新ui不刷新；

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        "1hello23456789",
+        textDirection: TextDirection.rtl,
+      ),
+    )
+//      Scaffold(
+//        appBar: AppBar(
+//          title: Text('longpc'),
+//        ),
+//        body: Center(
+//          child:
+//            Text(
+//                'hello',
+//                textDirection: TextDirection.ltr,
+//            ),
+//        ),
+//      )
+  );
+}
+
+
+
